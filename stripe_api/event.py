6a61953b30e4e6a867691705483307ea8f39f496
marian <marian.diaconu@bluetensor.ai>
2023-08-10 22:00:57 +0200

    Add Subscription events

--- stripe_api/event.py
+++ stripe_api/event.py
@@ -5,6 +5,9 @@ class EventType(Enum):
     PAYMENT_INTENT_SUCCEEDED = 'payment_intent.succeeded'
     PAYMENT_INTENT_FAILED = 'payment_intent.payment_failed'
     PAYMENT_INTENT_CREATED = 'payment_intent.created'
+    CUSTOMER_SUBSCRIPTION_CREATED = 'customer.subscription.created'
+    CUSTOMER_SUBSCRIPTION_UPDATED = 'customer.subscription.updated'
+    CUSTOMER_SUBSCRIPTION_DELETED = 'customer.subscription.deleted'
     UNKNOWN_EVENT = 'unknown'
 
 
